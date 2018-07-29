816faec7f1bc26589a2c9d232713b680e3f27dfc
Kai Boschung <boschung91@gmail.com>
2018-07-29 11:51:01 +0200

    bla

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '600082662:AAF9VWyKLlRCD9gkwcFuLz4mfyq2kMXiZgo';
+const token = process.env.TELEGRAM_TOKEN;
+console.log(token);
+//const token = '600082662:AAF9VWyKLlRCD9gkwcFuLz4mfyq2kMXiZgo';
 
 
 // Imports the Google Cloud client library
@@ -7,8 +9,16 @@ const vision = require('@google-cloud/vision');
 
 // Creates a client
 const client = new vision.ImageAnnotatorClient({
-    keyFilename: './My Project-c7fea86027a8.json'
+    projectId: process.env.GOOGLE_PROJECTID,
+    credentials: {
+        client_email: process.env.GOOGLE_CLIENTEMAIL,
+        private_key: process.env.GOOGLE_PRIVATEKEY,
+    }
 });
+
+// const client = new vision.ImageAnnotatorClient({
+//     keyfile: './My Project-2f082c08b8c1.json'
+// });
 
 const bot = new TelegramBot(token, {
     polling: true
@@ -56,7 +66,7 @@ bot.onText(/\/start/, (msg) => {
 
 bot.on("photo", (msg) => {
     let photoId = msg.photo[msg.photo.length - 1].file_id;
-    bot.downloadFile(photoId, "D:\\").then(path => {
+    bot.downloadFile(photoId, "./").then(path => {
         client
             .textDetection(path)
             .then(results => {
